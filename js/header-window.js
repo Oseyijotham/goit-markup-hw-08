526d8f4fe82282b46f24202e7c9fb0681fc6fd7d
Oseyijotham <jothamoboh16@gmail.com>
2024-08-25 14:32:59 +0100

    Update Site

--- js/header-window.js
+++ js/header-window.js
@@ -1,8 +1,10 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[header-window-open]'),
-    closeModalBtn: document.querySelector('[header-window-close]'),
-    modal: document.querySelector('[header-window]'),
+    openModalBtn: document.querySelector("[header-window-open]"),
+    closeModalBtn: document.querySelector("[header-window-close]"),
+    siteBody: document.querySelector("body"),
+    headerWindow: document.querySelector(".header-window"),
+    modal: document.querySelector("[header-window]"),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +12,7 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('menu__is-hidden');
+    refs.siteBody.classList.toggle("body__no-scroll");
+    //refs.headerWindow.classList.toggle("header-window__scroll");
   }
 })();
